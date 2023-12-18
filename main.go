779e8696cc3ab9f0d85655ed7ad8b93f812a9d14
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-18 22:35:27 +0000

    Render pages as markdown

--- main.go
+++ main.go
@@ -2,27 +2,106 @@ package main
 
 import (
 	"bloggo/logging"
+	"bloggo/templates"
+	"bytes"
 	"context"
-	"net/http"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
 
-	hello "bloggo/templates"
+	highlighting "github.com/yuin/goldmark-highlighting/v2"
+
+	"github.com/knadh/koanf/parsers/toml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/v2"
 )
 
+var conf = koanf.New(".")
+var logger = logging.New()
+
+var md = goldmark.New(
+	goldmark.WithExtensions(extension.GFM, highlighting.Highlighting),
+	goldmark.WithParserOptions(
+		parser.WithAutoHeadingID(),
+	),
+	goldmark.WithRendererOptions(
+		html.WithHardWraps(),
+		html.WithXHTML(),
+	),
+)
+
+func toHtml(filename string) (string, error) {
+	input, err := os.ReadFile(filename)
+
+	if err != nil {
+		return "", err
+	}
+
+	var html bytes.Buffer
+	if err := md.Convert(input, &html); err != nil {
+		logger.Errorf("Failed to convert %v", err)
+	}
+
+	return html.String(), nil
+}
+
+func dirToHtml(dir string) []string {
+	files, err := os.ReadDir(dir)
+
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	var htmlFiles []string
+	for _, file := range files {
+		if !file.IsDir() {
+			html, err := toHtml(filepath.Join(dir, file.Name()))
+
+			if err != nil {
+				logger.Fatal(err)
+			}
+
+			htmlFiles = append(htmlFiles, html)
+		}
+	}
+
+	return htmlFiles
+}
+
 func main() {
+	if err := conf.Load(file.Provider("config.toml"), toml.Parser()); err != nil {
+		logger.Fatalf("Error loding config: %v", err)
+	}
+
+	posts_directory := conf.String("directories.posts")
+
+	html := dirToHtml(posts_directory)
+
+	logger.Info(html)
+
 	server := echo.New()
-	logger := logging.New()
 
 	server.Use(logging.LogMiddleware(logger))
 
 	server.GET("/", func(ctx echo.Context) error {
-		return ctx.String(http.StatusOK, "Hello, world!")
+		return templates.Index().Render(context.Background(), ctx.Response().Writer)
 	})
 
-	server.GET("/hello/:name", func(ctx echo.Context) error {
-		name := ctx.Param("name")
-		return hello.Hello(name).Render(context.Background(), ctx.Response().Writer)
+	server.GET("/post/:id", func(ctx echo.Context) error {
+		id, err := strconv.Atoi(ctx.Param("id"))
+
+		if err != nil {
+			log.Fatal("Failed to convert ID to string.")
+		}
+
+		return templates.Post(html[id]).Render(context.Background(), ctx.Response().Writer)
 	})
 
 	logger.Fatal(server.Start(":1323"))
